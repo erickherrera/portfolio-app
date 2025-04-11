ff0fc97b2bbc74375c0f18a9efd4647229efac50
Erick Herrera <erickmherrera01@gmail.com>
2025-04-11 15:35:24 -0600

    modified:   src/app/ThemeToggle.js

--- src/app/ThemeToggle.js
+++ src/app/ThemeToggle.js
@@ -4,13 +4,14 @@ import { useTheme, THEMES } from './ThemeContext';
 import { useEffect, useState, useMemo } from 'react';
 
 // Move SVG icons outside the component
-const SunIcon = () => (
+const SunIcon = ({ isDarkMode, className }) => (
   <svg 
     xmlns="http://www.w3.org/2000/svg" 
     viewBox="0 0 24 24" 
-    fill="none" 
+    fill={isDarkMode ? "#171717" :"none"}
     strokeWidth={1.5} 
-    stroke="currentColor"
+    stroke={isDarkMode ? "#ededed" : "currentColor"}
+    className={className}
   >
     <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
   </svg>
@@ -29,13 +30,14 @@ const MoonIcon = ({ isDarkMode, className }) => (
   </svg>
 );
 
-const ComputerIcon = () => (
+const ComputerIcon = ({ isDarkMode, className }) => (
   <svg 
     xmlns="http://www.w3.org/2000/svg" 
     viewBox="0 0 24 24" 
-    fill="none" 
+    fill={isDarkMode ? "#171717" :"none"}
     strokeWidth={1.5} 
-    stroke="currentColor"
+    stroke={isDarkMode ? "#ededed" : "currentColor"}
+    className={className}
   >
     <path strokeLinecap="round" strokeLinejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25" />
   </svg>
