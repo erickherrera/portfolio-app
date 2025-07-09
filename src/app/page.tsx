67d70cdca2d9bc03d22da7ccfa78e3c09a8097b5
Erick Herrera <erickmherrera01@gmail.com>
2025-07-09 11:10:29 -0600

    modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -327,7 +327,7 @@ export default function Home() {
               className="mb-8 text-base sm:text-lg md:text-xl font-semibold px-2 break-words"
               style={{ color: colors.foreground}}
             >
-              More about my software engineering journey.
+              Learn more about my software engineering journey.
             </h2>
             
             <div className="flex flex-col sm:flex-row gap-4 sm:gap-6 mt-6 w-full max-w-sm justify-center">
